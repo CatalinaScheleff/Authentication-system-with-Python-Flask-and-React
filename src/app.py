e502b7a8c0b5b6f8c93c3b076c096aeab95efe41
CatalinaScheleff <122483646+CatalinaScheleff@users.noreply.github.com>
2023-04-26 01:47:28 +0000

    start

--- src/app.py
+++ src/app.py
@@ -10,8 +10,11 @@ from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
 #from models import Person
+import datetime 
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
+jwt=JWTManager(app)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
@@ -44,6 +47,33 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route("/login", methods=['POST'])
+def login():
+    body = request.get_json()
+    user = User.query.filter_by(email=body['email']).first()
+    if(user is None):
+        return jsonify({"mensaje": "el usuario no existe"}),404
+    else:
+        if (user.password != body['password']):
+            return "la contraseña esta mala"
+        else:
+            expiracion = datetime.timedelta(minutes=1)
+            token = create_access_token(identity=user.serialize(), expires_delta=expiracion)
+            return jsonify({
+                "mensaje": "bienvenido!",
+                "token": token,
+                "tiempo": expiracion.total_seconds(),
+                "data": user.serialize()
+            })
+
+@app.route("/check", methods=['GET'])
+@jwt_required()
+def check_user():
+    return jsonify({
+        "logeado": True
+    })
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
